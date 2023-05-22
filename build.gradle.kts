c8b9d0b96d1f19cf1a9cd4f49602529e8209cf24
Sungho95 <>
2023-05-22 15:26:47 +0900

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,13 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+
+    // exposed
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    // H2 database
     implementation("com.h2database:h2:$h2_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
